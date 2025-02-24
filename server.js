2dd191651ecf5be17b11eec887d43f01160e982d
Narasing <murapakanarasingarao02@gmail.com>
2025-02-24 21:18:13 +0530

    changes in package.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const rahadMediaDownloader = require('rahad-media-downloader');
 
 const app = express();
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;  
 
 // Middleware
 app.use(cors());
